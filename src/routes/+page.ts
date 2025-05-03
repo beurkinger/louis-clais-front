1677030654fc77b6d0fcc7260da8a9327c69cac5
Thibault Goehringer <tgoehringer@gmail.com>
2025-05-03 22:56:08 +0200

    rm fetch options

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -16,21 +16,19 @@ const { projectId, dataset } = client.config();
 
 const NUM_POSTS_PER_PAGE = 2;
 
-const options = { next: { revalidate: 30 } };
-
 const POST_FILTER = `[_type == "post" && defined(slug.current)]`;
 
 export const load: PageLoad = async ({ url }) => {
     const pageParam = Number(url.searchParams.get('p') || 0);
 
     const postsCountQuery = `count(*${POST_FILTER})`;
-    const postsCount = await client.fetch<number>(postsCountQuery, {}, options);
+    const postsCount = await client.fetch<number>(postsCountQuery, {});
 
     const pageId = (isNaN(pageParam) || ((pageParam * NUM_POSTS_PER_PAGE + 1) > postsCount)) ? 0 : pageParam;
     const offset = pageId * NUM_POSTS_PER_PAGE;
 
     const postsQuery = `*${POST_FILTER}|order(publishedAt desc)[${offset}...${offset + NUM_POSTS_PER_PAGE}]{_id, title, slug, publishedAt, gallery, body}`;
-    const posts = await client.fetch<Post[]>(postsQuery, {}, options);
+    const posts = await client.fetch<Post[]>(postsQuery, {});
 
     if (!dataset || !projectId || !posts || !postsCount) {
         throw error(500);
